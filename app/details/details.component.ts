cd750880ab6bd416226e655a63ac1dbd065e27f7
AlexandrLezh <sasha_l@inbox.lv>
2024-08-16 20:16:42 +0300

    hardcoded json moved to json-server

--- app/details/details.component.ts
+++ app/details/details.component.ts
@@ -14,34 +14,33 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   ],
   template: `
     <article>
-      <img class="listing-photo" [src]="housingLocation?.photo" alt="Exterior photo of {{housingLocation?.name}}"/>>
+      <img class="listing-photo"
+        [src]="housingLocation?.photo"
+        alt="Exterior photo of {{housingLocation?.name}}">
       <section class="listing-description">
-        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
-        <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
       </section>
-
       <section class="listing-features">
-      <h2 class="section-heading">About this housing location</h2>
+        <h2 class="section-heading">About this housing location</h2>
         <ul>
-          <li>Units available: {{ housingLocation?.availableUnits }}</li>
-          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
-          <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+          <li>Units available: {{housingLocation?.availableUnits}}</li>
+          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
+          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
         </ul>
       </section>
-
       <section class="listing-apply">
         <h2 class="section-heading">Apply now to live here</h2>
-        <form [formGroup]="applyForm" (submit)="submitApplication()" >
-          <label for="firstName">First Name</label>
-          <input type="text" id="first-name" formControlName="firstName">
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
 
-          <label for="lastName">Last Name</label>
-          <input type="text" id="last-name" formControlName="lastName">
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
 
           <label for="email">Email</label>
-          <input type="email" id="email" formControlName="email">
-
-          <button class="primary" type="submit">Apply now</button>
+          <input id="email" type="email" formControlName="email">
+          <button type="submit" class="primary">Apply now</button>
         </form>
       </section>
     </article>
@@ -60,7 +59,9 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   submitApplication() {
