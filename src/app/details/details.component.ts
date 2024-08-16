fbcb548ff85dd002c34ba26fc6d842f5ff9f8cd8
AlexandrLezh <sasha_l@inbox.lv>
2024-08-16 19:17:23 +0300

    Form Added

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,19 +4,22 @@ import { ActivatedRoute } from '@angular/router';
 import { HousingLocation } from '../housing-location';
 import { HousingService } from '../housing.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { last } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   template: `
     <article>
-      <img class="listing-photo" [src]="housingLocation?.photo">
+      <img class="listing-photo" [src]="housingLocation?.photo" alt="Exterior photo of {{housingLocation?.name}}"/>>
       <section class="listing-description">
         <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
         <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
       </section>
+
       <section class="listing-features">
       <h2 class="section-heading">About this housing location</h2>
         <ul>
@@ -25,9 +28,21 @@ import { last } from 'rxjs';
           <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
         </ul>
       </section>
+
       <section class="listing-apply">
-        <h2 class="section-heading">Apply now to live</h2>
-        <button class="primary" type="button">Apply</button>
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()" >
+          <label for="firstName">First Name</label>
+          <input type="text" id="first-name" formControlName="firstName">
+
+          <label for="lastName">Last Name</label>
+          <input type="text" id="last-name" formControlName="lastName">
+
+          <label for="email">Email</label>
+          <input type="email" id="email" formControlName="email">
+
+          <button class="primary" type="submit">Apply now</button>
+        </form>
       </section>
     </article>
   `,
@@ -35,16 +50,24 @@ import { last } from 'rxjs';
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  HousingService = inject(HousingService);
+  housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
   applyForm = new FormGroup({
-    fistName: new FormControl(''),
+    firstName: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
   });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.HousingService.getHousingLocationById(housingLocationId);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
